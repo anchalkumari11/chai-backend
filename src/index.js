812980cf47b20b7f14b9f1de865b58adebfe899f
Anchal Kumari <anchaljha468@gmail.com>
2025-11-03 22:05:21 +0530

    util code clearity

--- src/index.js
+++ src/index.js
@@ -5,5 +5,12 @@ dotenv.config({
 })
 
 connectDB()
-//const PORT = process.env.PORT || 8000;
-//console.log(`Server running on: http://localhost:${PORT}`);
+.then(() => {
+  app.listen(process.env.PORT || 8000, () => {
+    console.log(`Server is running at port: ${process.env.PORT}`);
+  }) 
+})
+.catch((err) => {
+  console.log("MONGO db connection failed !!! ",err);
+})
+
